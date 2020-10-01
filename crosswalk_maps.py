81e616b53b6425040c7206eb3979f9c12ee7a529
lib-eric <49041735+lib-eric@users.noreply.github.com>
2020-09-30 22:02:33 -0500

    Update and rename Crosswalk_Maps.py to crosswalk_maps.py
    -Added imports to ready for going forward.

---
+++ crosswalk_maps.py
@@ -0,0 +1,20 @@
+# CSV creation and writing
+import os
+from pathlib import Path
+import csv
+# REGEX for formatting/clean-up
+import re
+
+# Third party libraries
+# MARC processing/extraction
+import pymarc
+from pymarc import MARCReader
+from pymarc import marc8_to_unicode
+
+
+# TODO: Start mapping process
+
+
+# Start
+def process_marc(extract_from=None, save_name=None):
+    pass
