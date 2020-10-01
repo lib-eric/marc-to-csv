80eaddf01fe065d486998aeb66cf7dba97a4f134
lib-eric <49041735+lib-eric@users.noreply.github.com>
2020-09-30 22:00:57 -0500

    UI redesign and code tweak
    -Updated UI to use tkinter frames
    -Started using codetags to help organize

--- ui.py
+++ ui.py
@@ -1,120 +1,224 @@
 # UI
 import tkinter as tk
 from tkinter import messagebox as tkMessagebox
+from tkinter import filedialog as tkFiledialog
 
 # Custom local libraries
-from Crosswalk_Dissertations import process_marc as dissertation
-# from Crosswalk_Maps
+from crosswalk_dissertations import process_marc as dissertations
+from crosswalk_maps import process_marc as maps
 
 
 class App:
+
+    # CLASS VARIABLE - ls_mapping_options
+    #   {"rb_display_text": "rb_value"} || {"display_text": "module_to_run"}
+    # Option to dynamically add mapping options for future additions.
+    ls_mapping_options = [
+        ("Dissertation", "dissertations"),
+        ("Maps", "maps")
+        ]
+
     def __init__(self):
+
         self.root = tk.Tk()
-        # Setting title
+
+        # Setting Title
         self.root.title("MARC-to-CSV")
-        # Setting window size
-        width=700
-        height=350
+
+        # Setting Window Size
+        width=300
+        height=120
+
+        # Centering window on screen
         screenwidth = self.root.winfo_screenwidth()
         screenheight = self.root.winfo_screenheight()
         alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
         self.root.geometry(alignstr)
         self.root.resizable(width=True, height=True)
+        
+        # Create Frames/Widgets
+        self.create_input_widgets()
+        self.create_mapping_widgets()
+        self.create_action_widgets()
+        
+        # Start mainloop
+        self.root.mainloop()
 
-        # Entry - User provided input for INPUT FILE
-        self.entry_source_path=tk.Entry(
-            self.root,
+    # Collect input via:
+    #   ENTRY - source file & BUTTON - filedialog
+    #   ENTRY - output file
+    def create_input_widgets(self):
+        
+        # FRAME - entry_frame1
+        #   Containing (1) Lbl (2) Entry (3) Btn
+        self.entry_frame1 = tk.Frame(self.root)
+        self.entry_frame1.pack(padx=1, pady=1, anchor="w")
+        
+        # LABLE-ENTRY - lbl_source
+        #   For entry_source_path
+        self.lbl_source = tk.Label(
+            self.entry_frame1,
             justify = "left",
-            text = "input_source",
+            text = "Input source path:"
+            )
+        self.lbl_source.pack(side="left")
+
+        # ENTRY - source_path_text
+        #   User provided input for path the input file
+        self.source_path_text = tk.StringVar()
+        self.entry_source_path = tk.Entry(
+            self.entry_frame1,
+            justify = "left",
+            textvariable = self.source_path_text,
             borderwidth = "1px"
             )
-        self.entry_source_path.place(x=150,y=40,width=430,height=25)
-        
-        lbl_source=tk.Label(
-            self.root,
-            justify = "center",
-            text = "Source Path:"
-            )
-        lbl_source.place(x=50,y=40,width=100,height=25)
+        self.entry_source_path.pack(side="left")
 
-        # Entry - User provided input for OUTPUT FILE
+        # BUTTON - btn_select_source
+        #   Open up filedialog to ask for file
+        self.btn_select_source=tk.Button(
+            self.entry_frame1,
+            justify = "center",
+            text = "Dialog",
+            command = self.command_btn_select_source
+            )
+        self.btn_select_source.pack(side="right", padx=5)
+
+        # FRAME - entry_frame2
+        #   Containing (1) Lbl (2) Entry
+        self.entry_frame2 = tk.Frame(self.root)
+        self.entry_frame2.pack(padx=1, pady=1, anchor="w")
+
+        # LABLE-ENTRY - lbl_output
+        #   For entry_output_path
+        self.lbl_output=tk.Label(
+            self.entry_frame2,
+            justify = "center",
+            text = "Output Name:"
+            )
+        self.lbl_output.pack(side="left", padx=10)
+
+        # ENTRY - entry_output_path
+        #   User provided input for output path/name
         self.entry_output_path=tk.Entry(
-            self.root,
+            self.entry_frame2,
             justify = "left",
             text = "input_destination",
             borderwidth = "1px"
             )
-        self.entry_output_path.place(x=150,y=80,width=430,height=25)
+        self.entry_output_path.pack(side="left")
 
-        lbl_output=tk.Label(
-            self.root,
-            justify = "center",
-            text = "Output Name:"
-            )
-        lbl_output.place(x=50,y=80,width=100,height=25)
 
-        # Radio Button - ()Dissertations, ()Maps
-        self.rad_option = tk.StringVar()
-        self.rad_option.set("dissertation")
-        self.ra=tk.Radiobutton(
-            self.root,
-            text="Dissertations",
-            variable=self.rad_option,
-            value="dissertation",
-            justify="left"
-            )
-        self.ra.place(x=150,y=150,width=150,height=50)
-        self.rb=tk.Radiobutton(
-            self.root,
-            text="Maps",
-            variable=self.rad_option,
-            value="map",
-            justify="left"
-            )
-        self.rb.place(x=150,y=200,width=150,height=50)
+    # Collect input via:
+    #   RADIOBUTTON - mapping options
+    def create_mapping_widgets(self):
+        
+        # FRAME - rad_frame
+        #   containing (1) radiobutton(s)
+        self.rad_frame = tk.Frame(self.root)
+        self.rad_frame.pack(padx=1, pady=1, anchor="s")
 
-        # Button - Start
+        # Radiobutton - ()Dissertations, ()Maps
+        self.var_rb_option = tk.StringVar()
+
+        # Dynamically build RadioButton(s) based on class variable for mapping options.
+        for display_text, rb_value in self.ls_mapping_options:
+            self.rb = tk.Radiobutton(
+                self.rad_frame,
+                variable=self.var_rb_option,
+                text = display_text,
+                value = rb_value
+                )
+            self.rb.pack(side="left")
+            self.var_rb_option.set(rb_value)
+
+
+    # Wait for user interaction via:
+    #   BUTTON - btn_start
+    def create_action_widgets(self):
+        # Frame - btn_frame
+        self.btn_frame = tk.Frame(self.root)
+        self.btn_frame.pack(padx=1, pady=1, anchor="s")
+        
+        # Button - btn_start
+        # Starts the process for running the appropriate mapping.
         self.btn_start=tk.Button(
-            self.root,
+            self.btn_frame,
             justify = "center",
             text = "START",
-            command = self.btn_start_command
+            command = self.command_btn_start
             )
-        self.btn_start.place(x=150,y=270,width=150,height=45)
-
-        # Button - Exit
-        self.btn_Exit=tk.Button(
-            self.root,
-            justify = "center",
-            text = "EXIT",
-            command = self.btn_Exit_command
-            )
-        self.btn_Exit.place(x=420,y=270,width=124,height=44)
-
-        # Start Mainloop
-        self.root.mainloop()
+        self.btn_start.pack(side="bottom", padx=10, pady=10)
 
 
-    def btn_start_command(self):
+    # COMMAND - command_btn_select_source
+    #   Attached - btn_select_source
+    #   What to do when btn is pressed.
+    def command_btn_select_source(self):
+        
+        # FILEDIALOG - fd_source_path
+        fd_source_path = tk.filedialog.askopenfilename(
+            initialdir="/",
+            title="Select a file",
+            filetypes=(("MARC Files","*.bib;*.mrc"),("All Files","*.*")))
+        # Update Entry Source Path Text
+        self.source_path_text.set(fd_source_path)
+
+
+    # COMMAND - command_btn_start
+    #   Attached - btn_start
+    #   What to do when btn is pressed.
+    def command_btn_start(self):
+        
         print("btn START clicked.")
 
+
+        # TODO: Add in error checking before continuing with processing.
+        #   -Check if file is already exists, ask if want to append or cancel.
+        
+        # TODO: Create dynamic 'kick-off' based on the CLASS VARIABLE.
+
+
+        # Capture user provide input and save to variables.
         input_path = self.entry_source_path.get()
         output_path = self.entry_output_path.get()
 
-        # Choose which instructions to use based on RADIO BOX
-        selected_option = self.rad_option.get()
+        # Choose which mapping to use based on RADIOBOX.
+        selected_option = self.var_rb_option.get()
 
-        if selected_option == "dissertation":
-            dissertation(extract_from=input_path, save_name=output_path)
-            tkMessagebox.showinfo('Complete', "Operation finished running.")
-        elif selected_option == "map":
-            # TO-DO
+        # HACK: Dynamically execute method based on ls_mapping_options to run
+        for mapping_text, mapping_to_run in self.ls_mapping_options:
+            if mapping_to_run == selected_option:
+                exec(
+                    """{mapping_to_run}(extract_from=r"{input_path}", save_name="{output_path}")
+                    """.format(mapping_to_run=mapping_to_run, input_path=input_path, output_path=output_path)
+                    )
+                tk.messagebox.showinfo(
+                'Complete',
+                "Mapping: '{mapping_text}' ran.".format(mapping_text=mapping_text)
+                )
+                break
+            else:
+                tk.messagebox.showinfo(
+                'Error',
+                "Please select a process from the radiobox"
+                )
 
-            tkMessagebox.showinfo('Complete', "Operation finished running.")
-        else:
-            tkMessagebox.showinfo('Error', "Please select a process from the radiobox")
 
-
-    def btn_Exit_command(self):
-        print("Exiting Application.")
-        self.root.destroy()
+        # if selected_option == "dissertations":
+        #     dissertations(extract_from=input_path, save_name=output_path)
+        #     tk.messagebox.showinfo(
+        #         'Complete',
+        #         "Operation finished running."
+        #         )
+        # elif selected_option == "maps":
+        #     maps()
+        #     tk.messagebox.showinfo(
+        #         'Complete',
+        #         "Operation finished running."
+        #         )
+        # else:
+        #     tk.messagebox.showinfo(
+        #         'Error',
+        #         "Please select a process from the radiobox"
+        #         )
